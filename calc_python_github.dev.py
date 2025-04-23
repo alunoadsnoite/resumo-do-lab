b5469a40dd61e39e17af8547f6f1e9548645ce71
alunoadsnoite <130872082+alunoadsnoite@users.noreply.github.com>
2025-04-23 18:16:43 -0300

    calculadora feita em python

---
+++ calc_python_github.dev.py
@@ -0,0 +1,12 @@
+# criando uma calculador em python no github.dev
+# Calculadora de Soma
+
+# Pede ao usuário os dois números
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Faz a soma
+resultado = numero1 + numero2
+
+# Exibe o resultado
+print("A soma é:", resultado)
